dd0af8d2c9a2247a83bf1708752695dc571f2818
dylan2510 <dylan2510.dev@gmail.com>
2020-11-14 17:27:40 +0800

    Add project files.

---
+++ hostedservice_rabbitmq/Services/RabbitMQConsumer.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hostedservice_rabbitmq.Services
+{
+    public class RabbitMQConsumer : IHostedService
+    {
+
+        private IConnection connection;
+        private IModel channel;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            RegisterRabbitMQConsumer();
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            DeRegister();
+            return Task.CompletedTask;
+        }
+
+        private void RegisterRabbitMQConsumer()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+
+            // Declare the queue channel for listening
+            channel.QueueDeclare(queue: "rabbit",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+            // Create consumer
+            var consumer = new EventingBasicConsumer(channel);
+
+            // Consumer handler for message recieved
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Debug.WriteLine($"Received Message: {message}");
+            };
+
+            // Attach handler to queue
+            channel.BasicConsume(queue: "rabbit",
+                             autoAck: true,
+                             consumer: consumer);
+
+        }
+
+        public void DeRegister()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ hostedservice_rabbitmq/Services/RabbitMQProducer.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hostedservice_rabbitmq.Services
+{
+    public class RabbitMQProducer : IHostedService
+    {
+        private IModel channel;
+        private Timer _timer;
+
+        public RabbitMQProducer()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            RegisterRabbitMQ();
+            RegisterTimerPublishEvent();
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private void RegisterRabbitMQ()
+        {
+            // Create a Queue to publish messages to
+            channel.QueueDeclare(
+                queue: "rabbit",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+
+        private void PublishEvent(object state)
+        {
+            // Create message body
+            string message = $"{DateTime.Now} : Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            // Publish message to queue
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "rabbit",
+                basicProperties: null,
+                body: body
+                );
+        }
+
+        private void RegisterTimerPublishEvent()
+        {
+            // Publish to RabbitMQ Queue every 5 seconds
+            _timer = new Timer(PublishEvent, null, 0, 5000);
+        }
+    }
+}
